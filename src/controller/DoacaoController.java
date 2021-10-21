e4ebf738b2560003b9464420de19b4ae840c6101
Nayara Souza <nayara.souza@ccc.ufcg.edu.br>
2021-10-21 14:50:03 -0300

    acrescentado verificador de possiveis erros no metodo match()

--- src/controller/DoacaoController.java
+++ src/controller/DoacaoController.java
@@ -10,66 +10,21 @@ import java.util.List;
 public class DoacaoController {
 
     private final Validacao valida = new Validacao();
-    private ItemController itemController;
-    private List<Doacao> doacoes;
+    private final ItemController itemController;
+    private final List<Doacao> doacoes;
 
     public DoacaoController(ItemController itemController) {
         this.itemController = itemController;
         this.doacoes = new ArrayList<Doacao>();
     }
 
-    private Doador verificaItemDoadoExiste(int idItem) {
-        Doador doador = itemController.encontraDoadorPorIdItem(idItem);
-        if (doador == null) {
-            valida.itemNaoEncontrado(idItem);
-        }
-        return doador;
-    }
-
-    private Receptor verificaItemNecessarioExiste(int idItem) {
-        Receptor receptor = itemController.encontraReceptorPorIdItem(idItem);
-        if (receptor == null) {
-            valida.itemNaoEncontrado(idItem);
-        }
-        return receptor;
-    }
-
-    // Os valores se invertem pois depois de utilizar o método modificaQuantidadeItens() os valores dos itens se alteram
-    private void removeItensDoados(Item itemDoado, Item itemNecessario, Receptor receptor, Doador doador) {
-        if (itemDoado.getQuantidade() < itemNecessario.getQuantidade()) {
-            receptor.removeItembyid(itemNecessario.getId());
-        }
-        else if (itemDoado.getQuantidade() > itemNecessario.getQuantidade()) {
-            doador.removeItembyid(itemDoado.getId());
-        }
-        else {
-            receptor.removeItembyid(itemNecessario.getId());
-            doador.removeItembyid(itemDoado.getId());
-        }
-    }
-
-    private void modificaQuantidadeItens(Item itemDoado, Item itemNecessario, Doacao doacao, int novaQuantidadeItens) {
-        if (itemDoado.getQuantidade() > itemNecessario.getQuantidade()) {
-            doacao.setQuantidadeDoada(itemNecessario.getQuantidade());
-            itemDoado.setQuantidade(novaQuantidadeItens);
-        }
-        else if (itemDoado.getQuantidade() < itemNecessario.getQuantidade()) {
-            doacao.setQuantidadeDoada(itemDoado.getQuantidade());
-            itemNecessario.setQuantidade(novaQuantidadeItens);
-        }
-        else {
-            doacao.setQuantidadeDoada(itemNecessario.getQuantidade());
-        }
-    }
-
-
     public String realizaDoacao(int idItemNecessario, int idItemDoado, String data) {
         valida.verificaIdItem(idItemDoado);
         valida.verificaIdItem(idItemNecessario);
         valida.verificaData(data);
 
-        Receptor receptor = verificaItemNecessarioExiste(idItemNecessario);
-        Doador doador = verificaItemDoadoExiste(idItemDoado);
+        Receptor receptor = encontraReceptorPorIdItem(idItemNecessario);
+        Doador doador = encontraDoadorPorIdItem(idItemDoado);
 
         Item itemDoado = doador.getItemById(idItemDoado);
         Item itemNecessario = receptor.getItemById(idItemNecessario);
@@ -79,13 +34,15 @@ public class DoacaoController {
         Doacao doacao = new Doacao(doador, receptor, 0, data, itemNecessario.getDescricao());
 
         int novaQuantidadeItens = Math.abs(itemNecessario.getQuantidade() - itemDoado.getQuantidade());
+        boolean maiorQuantidadeItemDoado = itemDoado.getQuantidade() > itemNecessario.getQuantidade();
+        boolean maiorQuantidadeItemNecessario = itemDoado.getQuantidade() < itemNecessario.getQuantidade();
 
-        if (itemDoado.getQuantidade() > itemNecessario.getQuantidade()) {
+        if (maiorQuantidadeItemDoado) {
             doacao.setQuantidadeDoada(itemNecessario.getQuantidade());
             itemDoado.setQuantidade(novaQuantidadeItens);
             receptor.removeItembyid(idItemNecessario);
         }
-        else if (itemDoado.getQuantidade() < itemNecessario.getQuantidade()) {
+        else if (maiorQuantidadeItemNecessario) {
             doacao.setQuantidadeDoada(itemDoado.getQuantidade());
             itemNecessario.setQuantidade(novaQuantidadeItens);
             doador.removeItembyid(idItemDoado);
@@ -96,10 +53,8 @@ public class DoacaoController {
             doador.removeItembyid(idItemDoado);
         }
 
-        //modificaQuantidadeItens(itemDoado, itemNecessario, doacao, novaQuantidadeItens);
-        //removeItensDoados(itemDoado, itemNecessario, receptor, doador);
-
         doacoes.add(doacao);
+
 
         return doacao.toString();
     }
@@ -115,4 +70,85 @@ public class DoacaoController {
         return saida.substring(0, saida.length() - 3);
     }
 
+    public String match(String idReceptor, int idItemNecessario) {
+        valida.verificaIdItem(idItemNecessario);
+        valida.verificaIdUsuario(idReceptor);
+
+        Receptor receptor = encontraReceptorPorIdItem(idItemNecessario);
+        Item itemNecessario = receptor.getItemById(idItemNecessario);
+
+        String saida = "";
+        List<Item> list = new ArrayList<Item>();
+        for (String u : itens.keySet()) {
+            for (Integer i : itens.get(u).keySet()) {
+                int pontuacao = 0;
+                if (itens.get(u).get(i).getUser().getStatus().equals("doador")) {
+                    if (itens.get(u).get(i).getDescricao().equals(itens.get(idReceptor).get(idItemNecessario).getDescricao())) {
+                        pontuacao += 20;
+                        for (String tag : itens.get(u).get(i).getTags().split(",")) {
+                            for(String tagItem : itens.get(idReceptor).get(idItemNecessario).getTags().split(",")) {
+                                if (itens.get(idReceptor).get(idItemNecessario).getTags().split(",").length <= itens.get(u).get(i).getTags().split(",").length) {
+                                    for (int j = 0; j < itens.get(u).get(i).getTags().split(",").length; j++) {
+                                        if (tagItem.contentEquals(tag)) {
+                                            pontuacao += 10;
+                                        }
+                                        else if (tagItem.contains(tag)) {
+                                            pontuacao += 5;
+                                        }
+                                    }
+                                }else if (itens.get(idReceptor).get(idItemNecessario).getTags().split(",").length > itens.get(u).get(i).getTags().split(",").length) {
+                                    for (int j = 0; j < itens.get(idReceptor).get(idItemNecessario).getTags().split(",").length; j++) {
+                                        if (tagItem.contentEquals(tag)) {
+                                            pontuacao += 10;
+                                        }
+                                        else if (tagItem.contains(tag)) {
+                                            pontuacao += 5;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        itens.get(u).get(i).setPontuacaoMatch(pontuacao);
+                        list.add(itens.get(u).get(i));
+                    }
+                }
+            }
+        }
+        Collections.sort(list, Item.comparaPontuacaoMatch);
+        for (int i = 0; i < list.size(); i++) {
+            saida += list.get(i).toString() + ", doador: " + list.get(i).getUser().getNome() + "/" + list.get(i).getUser().getId();
+            saida += " | ";
+        }
+        if (saida.equals("")) {
+            return "";
+        }
+        return saida.substring(0, saida.length() - 3);
+    }
+
+
+    private Doador encontraDoadorPorIdItem(int idItem) {
+        Doador doador = itemController.encontraDoadorPorIdItem(idItem);
+        if (doador == null) {
+            Receptor receptor = encontraReceptorPorIdItem(idItem);
+            if (receptor != null) {
+                throw new IllegalArgumentException("O Usuario deve ser um doador: " + receptor.getId()+ ".");
+            } else {
+                valida.itemNaoEncontrado(idItem);
+            }
+        }
+        return doador;
+    }
+
+    private Receptor encontraReceptorPorIdItem(int idItem) {
+        Receptor receptor = itemController.encontraReceptorPorIdItem(idItem);
+        if (receptor == null) {
+            Doador doador = encontraDoadorPorIdItem(idItem);
+            if (doador != null) {
+                throw new IllegalArgumentException("O Usuario deve ser um receptor: " + doador.getId()+ ".");
+            } else {
+                valida.itemNaoEncontrado(idItem);
+            }
+        }
+        return receptor;
+    }
 }
